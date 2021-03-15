a89e37cc95dfea7623a447846a1808df6d1455e3
Brice Schultz <Briceschutz91@gmail.com>
2021-03-15 15:20:01 -0700

    Lots of Small Changes / Created Navbar Component, Going home | TS @ 21:31

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Layout.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name="keywords" content="Web Development, programming" />
+      </Head>
+      <h1>Welcome to Next JS</h1>
+    </div>
+  );
 }
