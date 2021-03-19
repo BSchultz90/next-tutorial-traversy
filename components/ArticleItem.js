e912a1ada6d7b6417d0c30137daa1c398bbdc33f
Brice Schultz <Briceschutz91@gmail.com>
2021-03-18 21:24:03 -0700

    Lots of Changes / Lazy Commit

---
+++ components/ArticleItem.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import articleStyles from "../styles/Article.module.css";
+
+// Below the Link Component has an href of /article/[id] and the as= alias's the article.id variable in place of the written path
+// The <a> tag within the Link component will route to the specified path.
+const ArticleItem = ({ article }) => {
+  return (
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <h3>{article.title} &arr;</h3>
+        <p>{article.body}</p>
+      </a>
+    </Link>
+  );
+};
+
+export default ArticleItem;
